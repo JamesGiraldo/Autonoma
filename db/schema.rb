209de12d8d8390d3a5e84adfaf29c21bae6d2f96
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-02-12 17:06:00 -0500

    coommitt
    laion cv

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20200212190359) do
     t.string "direccion"
     t.bigint "documento"
     t.string "descripcion"
+    t.string "perfil"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

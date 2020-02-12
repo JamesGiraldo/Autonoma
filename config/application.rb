407cf6e3bd3cd829f03f7d511c286007b73545eb
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-02-11 22:41:45 -0500

    New Proyect

---
+++ config/application.rb
@@ -0,0 +1,34 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Autonoma
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value.to_s
+      end if File.exists?(env_file)
+    end
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.to_prepare do
+      Devise::SessionsController.layout "session"
+      Devise::PasswordsController.layout "session"
+      Devise::PasswordsController.layout "session"
+      Devise::ConfirmationsController.layout "session"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "session" }
+      # Or to configure mailer layout
+      # Devise::Mailer.layout "email" # email.haml or email.erb
+    end
+  end
+end

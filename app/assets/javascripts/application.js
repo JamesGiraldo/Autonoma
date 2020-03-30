721f8691e3f4c3861ac1cceb4ab8d855c3699d13
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-03-30 14:39:23 -0500

    scrollTop Listo :v

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,3 +29,20 @@
 //= require apexcharts
 //= require_tree .
 $('#datepicker').datepicker();
+
+
+$(document).ready(function(){
+    $('#ir-arriba').click(function(){
+      $('body, html').animate({
+          scrollTop: '0px'
+        }, 300);
+      });
+
+      $(window).scroll(function(){
+        if( $(this).scrollTop() > 0 ){
+            $('#ir-arriba').slideDown(300);
+        } else {
+          $('#ir-arriba').slideUp(300);
+        }
+      });
+});

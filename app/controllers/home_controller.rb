563e1f9e1ac7543204e6e5cf42353c5848138d7e
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-04-15 19:49:09 -0500

    :v graficas Lineas y sus cursos . count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-
 class HomeController < ApplicationController
   before_action :authenticate_user!
-
   def index
     if current_user.has_role? :Admin
     else
@@ -10,9 +7,7 @@ class HomeController < ApplicationController
         flash[:info] = 'Sugerencia Asegurese De Actualizar Su datos!'
       end
     end
-    # @users = User.all.includes(:roles).where('roles.name' => "Docente").group_by_day(:created_at, format: "%a").count
-    # @male_user = Curso.includes(:linea).where(curso.linea_id = curso.id).group_by_week(:created_at).count
-    # @female_user = Linea.where(nombre: :ingles).group_by_day(:nombre).count
-    @cursos = Curso.all.includes(:linea).group_by_day_of_week(:created_at, format: '%a').count
+    @lineas = Linea.includes(:cursos).collect{|linea| [linea.nombre,
+                                                      linea.cursos.size]}
   end
 end

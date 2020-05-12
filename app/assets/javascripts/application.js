0ea8a80d9ed9fecfce40fd2262e024ef87f08a07
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-05-12 15:54:22 -0500

    listolla

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,20 +30,19 @@
 //= require_tree .
 $('#datepicker').datepicker();
 
-
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
-    $('#ir-arriba').click(function(){
-      $('body, html').animate({
-          scrollTop: '0px'
-        }, 300);
-      });
-
-      $(window).scroll(function(){
-        if( $(this).scrollTop() > 0 ){
-            $('#ir-arriba').slideDown(300);
-        } else {
-          $('#ir-arriba').slideUp(300);
-        }
-      });
+  $('#ir-arriba').click(function(e){
+    e.preventDefault();
+    $('body, html').animate({
+        scrollTop: '0px'
+      }, 300);
+    });
+    $(window).scroll(function(){
+      if( $(this).scrollTop() > 0 ){
+          $('#ir-arriba').slideDown(300);
+      } else {
+        $('#ir-arriba').slideUp(300);
+      }
+    });    
 });

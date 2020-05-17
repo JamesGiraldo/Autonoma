3f2968171245d6ee90e1d7dbda407dec44af551c
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-05-16 21:38:28 -0500

    menssage bienbenida en home de actualizacion de datos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    unless current_user.has_role? :Decano or current_user.has_role? :Vicerrector or current_user.has_role? :Director or current_user.has_role? :Docente
-        current_user.documento.present? or current_user.telefono.present? or current_user.nombre.present? or current_user.apellido.present? or current_user.perfil.present? or current_user.direccion.present?
-        flash[:info] = 'Sugerencia Asegurese De Actualizar Su datos!'
+    if current_user.documento.present? or current_user.telefono.present? or
+       current_user.nombre.present? or current_user.apellido.present? or
+       current_user.perfil.present? or current_user.direccion.present?
+    else
+      flash[:info] = 'Sugerencia Asegurese De Actualizar Su datos!'
     end
     @lineas = Linea.includes(:cursos).collect{|linea| [linea.nombre,
                                                       linea.cursos.size]}

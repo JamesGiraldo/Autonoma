bf221b922483012e2cefaa0877b6da55fe1dedfd
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-06-16 23:52:33 -0500

    validacionde home actualizacion requerida con flash-alert(tipo Info) lista

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,13 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    if current_user.documento.present? or current_user.telefono.present? or
-       current_user.nombre.present? or current_user.apellido.present? or
-       current_user.perfil.present? or current_user.direccion.present?
+    if current_user.documento.present? && current_user.telefono.present? &&
+       current_user.nombre.present? && current_user.apellido.present? &&
+       current_user.perfil.present? && current_user.direccion.present?
     else
       flash[:info] = 'Sugerencia Asegurese De Actualizar Su datos!'
     end
     @lineas = Linea.includes(:cursos).collect{|linea| [linea.nombre,
-                                                      linea.cursos.size]}
-    @users = User.all.includes(:roles).where('roles.name' => 'Docente').count
+                                                      linea.cursos.size]}       
   end
 end
